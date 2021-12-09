cc749398de7e27954d595e19dcecea0f9ad2d1f8
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:42:05 -0500

    Create BUBBLE_READY.vb

---
+++ api/examples/vb.net/BUBBLE_READY.vb
@@ -0,0 +1,8 @@
+<Serializable()>
+Public Class BUBBLE_READY
+	Public InstanceID As Long = 0
+	Public BubbleID As Long = 0
+	Public MountPoint As String = String.Empty
+  Public PreLaunch As New LAUNCH_RESULT
+	Public PostLaunch As New LAUNCH_RESULT
+End Class
