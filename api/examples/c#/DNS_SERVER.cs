b5d1b5632c5d737097c243bc2e11d3cd04e64cf4
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 16:17:36 -0500

    Update DNS_SERVER.cs

--- api/examples/c#/DNS_SERVER.cs
+++ api/examples/c#/DNS_SERVER.cs
@@ -3,8 +3,9 @@
 [Serializable()]
 public partial class DNS_SERVER
 {
-    public long ID = 0L;
+    public long ProviderID = 0L;
     public string Provider = string.Empty;
+    public long ServerID = 0L;
     public string Country = string.Empty;
     public int DNSType = 0;
     public string Address = string.Empty;
