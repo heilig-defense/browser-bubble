f0557e55d00300fb5a56e1da3b3aca1e9fbdeee7
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:51:49 -0500

    Create SPOOF_BATTERY.vb

---
+++ api/examples/vb.net/SPOOF_BATTERY.vb
@@ -0,0 +1,7 @@
+<Serializable()>
+Public Class SPOOF_BATTERY
+    Public Flags As Integer = 0
+    Public Values1 As Integer = 0
+    Public Values2 As Integer = 0
+    Public Values3 As Integer = 0
+End Class
