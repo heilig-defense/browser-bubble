7f114377f92d9e8cf06a573fd904aeaa9d121d92
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:47:54 -0500

    Create LOG_MESSAGE.vb

---
+++ api/examples/vb.net/LOG_MESSAGE.vb
@@ -0,0 +1,7 @@
+<Serializable()>
+Public Class LOG_MESSAGE
+	Public ErrorCode As Integer = 0
+	Public Level As Integer = 0
+	Public Location As String = String.Empty
+	Public Message As String = String.Empty
+End Class
