a1bc8193545489e0dce06dd6417c15c358453bee
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:40:21 -0500

    Create BUBBLE_GENERATE.vb

---
+++ api/examples/vb.net/BUBBLE_GENERATE.vb
@@ -0,0 +1,7 @@
+<Serializable()>
+Public Class BUBBLE_GENERATE
+    Public BrowserID As Long = 0
+    Public Count As Integer = 0
+    Public IconLocation As Integer = 0
+    Public IconPath As String = String.Empty
+End Class
