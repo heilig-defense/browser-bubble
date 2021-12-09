a50eafffc37cf0468d8311b30526092311c3d9e7
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:33:33 -0500

    Delete SERVER_REQUEST.vb

--- api/examples/vb.net/SERVER_REQUEST.vb
+++
@@ -1,7 +0,0 @@
-<Serializable()>
-Public Class ServerRequest
-    Public Version As Integer = 1
-    Public MessageId As Long = 0
-    Public DataType As Integer = 0
-    Public RequestValue As String = String.Empty
-End Class
