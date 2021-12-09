424dec044bcbbc7e35473073fb5900c27ae13718
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:57:41 -0500

    Create SPEECH_VOICE.vb

---
+++ api/examples/vb.net/SPEECH_VOICE.vb
@@ -0,0 +1,8 @@
+<Serializable()>
+Public Class SPEECH_VOICE
+    Public ID As Long = 0
+    Public Name As String = String.Empty
+    Public Language As LANGUAGE
+    Public Age As Integer = 0
+    Public Sex As Integer = 0
+End Class
