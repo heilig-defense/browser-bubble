3eec31f1a09999583346bcc731a47837d521515e
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:45:40 -0500

    Create DNS_SERVER.vb

---
+++ api/examples/vb.net/DNS_SERVER.vb
@@ -0,0 +1,9 @@
+<Serializable()>
+Public Class DNS_SERVER
+    Public ID As Long = 0
+    Public Provider As String = String.Empty
+    Public Country As String = String.Empty
+    Public DNSType As Integer = 0
+    Public Address As String = String.Empty
+    Public Port As Integer = 0
+End Class
