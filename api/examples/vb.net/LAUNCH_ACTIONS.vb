3c4155e65b52a3c1d351b9a4b1e4978e6d634314
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:37:55 -0500

    Create LAUNCH_ACTIONS.vb

---
+++ api/examples/vb.net/LAUNCH_ACTIONS.vb
@@ -0,0 +1,6 @@
+<Serializable()>
+Public Class LAUNCH_ACTIONS
+    Public Pre As String = String.Empty
+    Public Post As String = String.Empty
+    Public Shutdown As String = String.Empty
+End Class
