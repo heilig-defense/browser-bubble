f5df15894ec64084dfc2147d9fb04ac46a3c3123
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:43:39 -0500

    Create BUBBLE_SHORTCUT.vb

---
+++ api/examples/vb.net/BUBBLE_SHORTCUT.vb
@@ -0,0 +1,8 @@
+<Serializable()>
+Public Class BUBBLE_SHORTCUT
+    Public ID As Long = 0
+    Public Name As String = String.Empty
+    Public IsInstance As Integer = 0
+    Public IconLocation As Integer = 0
+    Public IconPath As String = String.Empty
+End Class
