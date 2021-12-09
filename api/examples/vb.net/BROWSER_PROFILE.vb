8202135d4df56945f84ddd35fde9f894fa144c7b
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:35:38 -0500

    Create BROWSER_PROFILE.vb

---
+++ api/examples/vb.net/BROWSER_PROFILE.vb
@@ -0,0 +1,9 @@
+<Serializable()>
+Public Class BROWSER_PROFILE
+    Public ID As Long = 0
+    Public Name As String = String.Empty
+    Public BrowserType As Integer = 0
+    Public FilePath As String = String.Empty
+    Public ProfilePath As String = String.Empty
+    Public Enabled As Integer = 0
+End Class
