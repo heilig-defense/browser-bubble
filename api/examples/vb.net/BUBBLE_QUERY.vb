90f2029ed29acc073c96c3cca2795b5c481c7882
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:41:33 -0500

    Create BUBBLE_QUERY.vb

---
+++ api/examples/vb.net/BUBBLE_QUERY.vb
@@ -0,0 +1,14 @@
+<Serializable()>
+Public Class BUBBLE_QUERY
+    Public BrowserID As Long = 0
+    Public BrowserPID As Integer = 0
+    Public DNSID As Long = 0
+    Public ProxyList As New List(Of Long)
+    Public VPNID As Long = 0
+    Public ProfilePath As String = String.Empty
+    Public PreLaunch As New LAUNCH_RESULT
+    Public PostLaunch As New LAUNCH_RESULT
+    Public Options As Integer = 0
+    Public BorderColor As Integer = 0
+    Public SpoofSettings As New Dictionary(Of Integer, String)
+End Class
