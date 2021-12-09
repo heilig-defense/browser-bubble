21188285ec6923a31267f0bdc6e22fecad81146a
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:49:48 -0500

    Create VPN_SERVER.vb

---
+++ api/examples/vb.net/VPN_SERVER.vb
@@ -0,0 +1,8 @@
+<Serializable()>
+Public Class VPN_SERVER
+    Public ID As Long = 0
+    Public Provider As String = String.Empty
+    Public Country As New COUNTRY
+    Public Address As String = String.Empty
+    Public Adapter As String = String.Empty
+End Class
