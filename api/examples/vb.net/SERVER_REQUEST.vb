8b8939a5ecc7d3e68c5a3da73fb89f37fc1d00d6
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:33:57 -0500

    Create SERVER_REQUEST.vb

---
+++ api/examples/vb.net/SERVER_REQUEST.vb
@@ -0,0 +1,7 @@
+<Serializable()>
+Public Class SERVER_REQUEST
+    Public Version As Integer = 1
+    Public MessageId As Long = 0
+    Public DataType As Integer = 0
+    Public RequestValue As String = String.Empty
+End Class
