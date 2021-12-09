8c782b409c0bb05103a3cf6578d17e416fd94bcc
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:33:15 -0500

    Create SERVER_RESPONSE.vb

---
+++ api/examples/vb.net/SERVER_RESPONSE.vb
@@ -0,0 +1,8 @@
+<Serializable()>
+Public Class SERVER_RESPONSE
+    Public ErrorCode As Integer = 0
+    Public DataType As Integer = 0
+    Public Version As Integer = 1
+    Public MessageID As Long = 0
+    Public ReturnValue As String = String.Empty
+End Class
