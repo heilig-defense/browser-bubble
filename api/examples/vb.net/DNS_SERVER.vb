e5cf5d16c7886baf65e9db25c43f91a8171c51ed
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 16:16:26 -0500

    Update DNS_SERVER.vb

--- api/examples/vb.net/DNS_SERVER.vb
+++ api/examples/vb.net/DNS_SERVER.vb
@@ -1,7 +1,8 @@
 <Serializable()>
 Public Class DNS_SERVER
-    Public ID As Long = 0
+    Public ProviderID As Long = 0
     Public Provider As String = String.Empty
+    Public ServerID As Long = 0
     Public Country As String = String.Empty
     Public DNSType As Integer = 0
     Public Address As String = String.Empty
