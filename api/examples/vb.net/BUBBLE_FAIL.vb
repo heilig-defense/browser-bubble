dad2a9234812f64d15739ede685296073461020a
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:39:39 -0500

    Create BUBBLE_FAIL.vb

---
+++ api/examples/vb.net/BUBBLE_FAIL.vb
@@ -0,0 +1,7 @@
+<Serializable()>
+Public Class BUBBLE_FAIL
+    Public ProcessId As Integer = 0
+    Public BrowserPath As String = String.Empty
+    Public ErrorCode As Integer = 0
+    Public Log As String = String.Empty
+End Class
