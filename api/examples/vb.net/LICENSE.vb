71bc8ad9aaf20e43ea324be5a7f8acc5e29c6508
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-09 07:47:34 -0500

    Create LICENSE.vb

---
+++ api/examples/vb.net/LICENSE.vb
@@ -0,0 +1,8 @@
+<Serializable()>
+Public Class LICENSE
+    Public LicenseKey As String = String.Empty
+    Public Status As Integer = 0
+    Public Expires As String = String.Empty
+    Public Entitlements As Long = 0
+    Public EndpointCount As Long = 0
+End Class
