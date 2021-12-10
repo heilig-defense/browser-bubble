bcf376584909404fa981bc90439217430d57e027
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-10 15:04:53 -0500

    Update BUBBLE_FAIL.vb

--- api/examples/vb.net/BUBBLE_FAIL.vb
+++ api/examples/vb.net/BUBBLE_FAIL.vb
@@ -1,5 +1,6 @@
 <Serializable()>
 Public Class BUBBLE_FAIL
+    Public InstanceID As Long = 0
     Public ProcessId As Integer = 0
     Public BrowserPath As String = String.Empty
     Public ErrorCode As Integer = 0
