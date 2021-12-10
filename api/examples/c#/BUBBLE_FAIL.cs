9a8db7a25a9d3dd7931f6d8718dda72a589bbc90
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2021-12-10 15:04:14 -0500

    Update BUBBLE_FAIL.cs

--- api/examples/c#/BUBBLE_FAIL.cs
+++ api/examples/c#/BUBBLE_FAIL.cs
@@ -3,6 +3,7 @@
 [Serializable()]
 public partial class BUBBLE_FAIL
 {
+    public long InstanceID = 0L;
     public int ProcessId = 0;
     public string BrowserPath = string.Empty;
     public int ErrorCode = 0;
