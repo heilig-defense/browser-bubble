84cf2250ca1da2232632ae013a77533ec766d722
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2022-01-21 20:20:25 -0500

    Update BUBBLE_RUNNING.cs

--- api/examples/c#/BUBBLE_RUNNING.cs
+++ api/examples/c#/BUBBLE_RUNNING.cs
@@ -5,5 +5,7 @@ public partial class BUBBLE_RUNNING
 {
     public long InstanceID = 0L;
     public long BubbleID = 0L;
+    public int DebugPort = 0;
+    public string DebugEndPoint = string.Empty;
     public long DTG = 0L;
 }
