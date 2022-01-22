a9f887624c8f23d82ef4447d038afb91eb967722
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2022-01-21 20:28:00 -0500

    Create BUBBLE_DEBUG_PORT.vb

---
+++ api/examples/vb.net/BUBBLE_DEBUG_PORT.vb
@@ -0,0 +1,8 @@
+Option Strict On
+
+<Serializable()>
+Public Class BUBBLE_DEBUG_PORT
+    Public InstanceID As Long = 0
+    Public Port As Integer = 0
+    Public WsEndPoint As String = String.Empty
+End Class
