d9d257513c7dec31949e521e6f331596e93e13a4
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2022-01-21 20:18:48 -0500

    Create BUBBLE_DEBUG_PORT.cs

---
+++ api/examples/c#/BUBBLE_DEBUG_PORT.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable()]
+public partial class BUBBLE_DEBUG_PORT
+{
+    public long InstanceID = 0L;
+    public int Port = 0;
+    public string WsEndPoint = string.Empty;
+}
