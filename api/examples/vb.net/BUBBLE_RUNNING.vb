410333ad1ea31fbe265dd5f9381fb0e6dc4b3c65
HeiDef <38450033+heilig-defense@users.noreply.github.com>
2022-01-21 20:27:21 -0500

    Update BUBBLE_RUNNING.vb

--- api/examples/vb.net/BUBBLE_RUNNING.vb
+++ api/examples/vb.net/BUBBLE_RUNNING.vb
@@ -2,5 +2,7 @@
 Public Class BUBBLE_RUNNING
     Public InstanceID As Long = 0
     Public BubbleID As Long = 0
+    Public DebugPort As Integer = 0
+    Public DebugEndPoint As String = String.Empty    
     Public DTG As Long = 0
 End Class
